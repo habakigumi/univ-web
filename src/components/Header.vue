3c7d0959c2467745c19142052d2881d0c2df896d
Taisei Nakajima <ne300085@senshu-u.jp>
2020-04-28 01:51:45 +0900

    nav layout

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,15 @@
 <template>
   <header>
-    <div class="project-name">
+    <div class="project-name text_sub">
       <h1>藤原プロジェクト2020</h1>
     </div>
     <nav class="menu">
       <ul>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Character</a></li>
-        <li><a href="#">Member</a></li>
-        <li><a href="#">Activities</a></li>
-        <li><a href="#">Link</a></li>
+        <li><a href="#" class="text_sub">About</a></li>
+        <li><a href="#" class="text_sub">Character</a></li>
+        <li><a href="#" class="text_sub">Member</a></li>
+        <li><a href="#" class="text_sub">Activities</a></li>
+        <li><a href="#" class="text_sub">Link</a></li>
       </ul>
     </nav>
   </header>
@@ -21,15 +21,34 @@ export default {
 }
 </script>
 
-<style scoped>
-  header {
-    width: 100%;
+<style scoped lang="scss">
+@import "../stylesheets/variables";
+
+header {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+nav.menu  {
+  font-size: 24px;
+  font-weight: bold;
+  width: 40%;
+  margin-right: 24px;
+  ul {
     display: flex;
     justify-content: space-between;
-  }
-
-  nav.menu ul {
-    display: flex;
     list-style: none;
+    li {
+      a {
+        text-decoration: none;
+      }
+    }
   }
+}
+
+h1 {
+  margin-left: 50px;
+}
 </style>
