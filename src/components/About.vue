21cadc3e763eabdec38900100dbdf97196dd0fe6
Taisei Nakajima <ne300085@senshu-u.jp>
2020-05-08 13:33:28 +0900

    create about content

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="about">
     <h2 class="heading_main"><span class="heading__main-text">About</span></h2>
+    <div class="about-container">
+      <div class="img"></div>
+      <div class="sentence">
+        <h3 class="text_sub">プロジェクトについて</h3>
+        <p class="text_black"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br>Fermentum in at curabitur dolor, metus, purus nulla
+          <br>
+          faucibus. Eu sit ultrices vulputate velit, habitant <br> faucibus tortor cras. Eu sed iaculis et potenti aliquam
+          <br>
+          dictum. Sed risus imperdiet eu leo sed.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
