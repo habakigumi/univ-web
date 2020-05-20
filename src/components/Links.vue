3ffefb38d9900527730f64eb51d5e67e24b2b010
Takahashi <ne300229@senshu-u.jp>
2020-05-21 03:27:54 +0900

    リンクアイコンを追加

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="links" id="links">
     <h2 class="heading_main"><span class="heading__main-text">Link</span></h2>
+    <div class="link_icons">
+      <a href="https://fjpj2020.blogspot.com/" target="_blank">
+        <font-awesome-icon :icon="['fab', 'blogger']" />
+      </a>
+      <a href="https://twitter.com/nevpj" target="_blank">
+        <font-awesome-icon :icon="['fab', 'twitter-square']" />
+      </a>
+      <a href="#" target="_blank">
+        <font-awesome-icon :icon="['fab', 'youtube-square']" />
+      </a>
+    </div>
   </div>
 </template>
 
