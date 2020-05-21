f73f2728ee5f552b0f6d5328e32985c9c94a9c39
Takahashi <ne300229@senshu-u.jp>
2020-05-21 17:07:43 +0900

    アイコンを円形に変更

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -3,13 +3,22 @@
     <h2 class="heading_main"><span class="heading__main-text">Link</span></h2>
     <div class="link_icons">
       <a href="https://fjpj2020.blogspot.com/" target="_blank" rel="noopener noreferrer">
-        <font-awesome-icon :icon="['fab', 'blogger']" />
+        <font-awesome-layers>
+          <font-awesome-icon :icon="['fas', 'circle']" />
+          <font-awesome-icon :icon="['fab', 'blogger-b']" class="icon" transform="shrink-5" />
+        </font-awesome-layers>
       </a>
       <a href="https://twitter.com/nevpj" target="_blank" rel="noopener noreferrer">
-        <font-awesome-icon :icon="['fab', 'twitter-square']" />
+        <font-awesome-layers>
+          <font-awesome-icon :icon="['fas', 'circle']" />
+          <font-awesome-icon :icon="['fab', 'twitter']" class="icon" transform="shrink-5" />
+        </font-awesome-layers>
       </a>
       <a href="#" target="_blank" rel="noopener noreferrer">
-        <font-awesome-icon :icon="['fab', 'youtube-square']" />
+        <font-awesome-layers>
+          <font-awesome-icon :icon="['fas', 'circle']" />
+          <font-awesome-icon :icon="['fab', 'youtube']" class="icon" transform="shrink-5 left-1" />
+        </font-awesome-layers>
       </a>
     </div>
   </div>
@@ -26,6 +35,9 @@
     a {
       font-size: 100px;
       color: $black;
+      .icon {
+        color: $background;
+      }
     }
     @media screen and (max-width: $break-point) {
       a {
