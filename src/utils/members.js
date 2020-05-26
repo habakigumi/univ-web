86391b32730e1cae4dbe9a4f7d389b7f49a8561c
taichanne30 <ne300209@senshu-u.jp>
2020-05-26 22:58:47 +0900

    Add members

--- src/utils/members.js
+++ src/utils/members.js
@@ -8,13 +8,13 @@ class Member {
 }
 export const members = [
   new Member('NE30-0006C', 'H0L1C', 'creator', 'H0L1C.png'),
-  new Member('NE30-0085F', 'Taisei', 'developer', ''),
+  new Member('NE30-0085F', 'Taisei', 'developer', 'tomaito.png'),
   new Member('NE30-0105K', 'あおき', 'creator', 'aoki.png'),
   new Member('NE30-0206B', 'Kentaro', 'planner', 'Kentaro.png'),
   new Member('NE30-0209G', 'taichan', 'developer', 'taichan.svg'),
   new Member('NE30-0223A', 'うさ', 'creator', 'usa.png'),
   new Member('NE30-0228B', 'haru', 'creator', 'haru.png'),
   new Member('NE30-0229K', 'めがね', 'developer', 'megane.png'),
-  new Member('NE30-0249B', 'いろはす', 'planner', ''),
+  new Member('NE30-0249B', 'いろはす', 'planner', 'irohasu.jpg'),
   new Member('担当教員', '藤原 正仁', 'teacher', 'fuji.png')
 ]
