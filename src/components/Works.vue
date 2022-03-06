63bd79d02347dc7841653c8ccf3c69164ffc05a5
Takahashi <ne300229@senshu-u.jp>
2022-03-07 00:57:14 +0900

    ライブ配信の内容追加、本年度→2020年度に置き換え、プロジェクトの成果説明文を過去形に変更、その他文章の調整

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -7,7 +7,7 @@
       <div class="text">
         <h3 class="text_sub">成果報告映像</h3>
         <p class="text_black">
-          本年度の藤原プロジェクトの活動とその成果をまとめた映像です。
+          2020年度の藤原プロジェクトの活動と、その成果をまとめた映像です。
         </p>
       </div>
       <div class="image">
@@ -24,8 +24,9 @@
       <div class="text">
         <h3 class="text_sub">動画シリーズ</h3>
         <p class="text_black">
-          YouTubeで公開している、天ノ葉つむぎと武者丸の動画シリーズです。<br>
-          本編は全11話、完結済です。
+          2020年度プロジェクトで制作した、天ノ葉つむぎと武者丸の動画シリーズです。<br>
+          日本にやってきた2人が元の世界に戻るまでを描いたオリジナルストーリーで、全11話です。<br>
+          <a href="https://youtube.com/playlist?list=PLBR98kmPGF9Az__V0vCjRMglP5URaxvk-" target="_blank" rel="noopener noreferrer">旧YouTubeチャンネル再生リスト</a>から視聴可能です。
         </p>
       </div>
       <div class="image">
@@ -40,11 +41,24 @@
 
     <div class="works_container">
       <div class="text">
+        <h3 class="text_sub">ライブ配信シリーズ</h3>
+        <p class="text_black">
+          2021年度卒業演習3･4からは、活動形態をライブ配信中心に切り替え、
+          視聴者と様々なコミュニケーションを行いました。<br>
+          身近なテーマを取り扱う雑談配信や、リアルタイムで絵を描く配信などの様々なテーマを企画しました。
+        </p>
+      </div>
+      <div class="image">
+        <img src="../assets/images/live_thumbnail.png">
+      </div>
+    </div>
+
+    <div class="works_container">
+      <div class="text">
         <h3 class="text_sub">オリジナルグッズ</h3>
         <p class="text_black">
-          つむぎと武者丸のキャラクターグッズを販売しています。
-          キャラクターグッズの定番「缶バッチ」や「ステッカー」の他、日常生活の役に立つ「サコッシュ」や「マグカップ」など様々な展開をしています。<br>
-          商品詳細・購入は <a href="https://suzuri.jp/_habaki_" target="_blank" rel="noopener noreferrer">こちら</a>
+          つむぎと武者丸のキャラクターグッズを販売しました。<br>
+          キャラクターグッズの定番「缶バッチ」や「ステッカー」の他、日常生活の役に立つ「サコッシュ」や「マグカップ」など様々な展開を行いました。<br>
         </p>
       </div>
       <div class="image">
@@ -56,8 +70,8 @@
       <div class="text">
         <h3 class="text_sub">LINEスタンプ</h3>
         <p class="text_black">
-          つむぎと武者丸のLINEスタンプを制作しました。描き下ろしイラスト40種入りです。<br>
-          その他のイラスト確認・購入は <a href="https://store.line.me/stickershop/product/14343630" target="_blank" rel="noopener noreferrer">こちら</a>
+          つむぎと武者丸のLINEスタンプを制作しました。<br>
+          「おはよう」「了解!!」など日常会話で使いやすいラインナップの描き下ろしイラスト40種入りです。<br>
         </p>
       </div>
       <div class="image">
@@ -69,7 +83,7 @@
       <div class="text">
         <h3 class="text_sub">制作記録本「VTuberの制作と技術」</h3>
         <p class="text_black">
-          一年間の活動の記録をまとめた本を制作しました。
+          2020年度の活動の記録をまとめた本を制作しました。
           印刷用に制作したデータをインターネット上で公開するために再編集したPDFファイルを公開しています。<br>
           見開き表示での閲覧を推奨しています。<br>
           DLは <a href="./download/HabakiBook_Interactive.pdf" download="藤原プロジェクト2020制作記録本「VTuberの制作と技術」.pdf">こちら</a>
