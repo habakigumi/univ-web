ca34a5c84aeaafa0fa09ae899f3ea27e0f32da17
Takahashi <ne300229@senshu-u.jp>
2022-03-12 13:18:25 +0900

    卒業演習の活動記録を追加

--- src/utils/activities.js
+++ src/utils/activities.js
@@ -27,5 +27,39 @@ export const activities = [
         content: '最終発表を実施。'
       }
     ]
+  },
+  {
+    year: '2021',
+    activities: [
+      {
+        month: '03',
+        content: '卒業演習3･4としての活動を開始。'
+      },
+      {
+        month: '05',
+        content: '人気VTuberの活動事例の調査を開始。'
+      },
+      {
+        month: '06',
+        content: 'ゲーム調査班・歌唱動画調査班・プロモーション調査班に分かれてより詳細な調査を開始。'
+      },
+      {
+        month: '07',
+        content: '卒業演習前期発表会に参加。'
+      },
+      {
+        month: '10',
+        content: 'ライブ配信活動を開始。'
+      }
+    ]
+  },
+  {
+    year: '2022',
+    activities: [
+      {
+        month: '01',
+        content: '卒業演習最終発表会に参加。'
+      }
+    ]
   }
 ]
